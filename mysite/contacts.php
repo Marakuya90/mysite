833be1be9698a3cc82cb1f5187c88fa1b782aad7
Marakuya90 <zhilzhil@mail.ru>
2023-04-29 23:12:03 +0300

    send mail,form

--- mysite/contacts.php
+++ mysite/contacts.php
@@ -4,11 +4,26 @@ $title = "Контакты";
 require "blocks/header.php";
 ?>
     <h1 class="mt-5"><?=$title?></h1>
+
+<div class="text-success"><?php if(!empty($_SESSION["success_mail"])) echo $_SESSION["success_mail"];?></div>
+
     <form action="./controllers/check_contact.php" method="POST" class="mt-5">
-        <input type="text" name="username" placeholder="Введите имя" value="<?=$_SESSION['username']?>" class="form-control"><br>
-        <input type="email" name="email" placeholder="Введите Email" value="<?=$_SESSION['email']?>" class="form-control"><br>
-        <input type="text" name="subject" placeholder="Тема сообщения" value="<?=$_SESSION['subject']?>" class="form-control"><br>
-        <textarea name="message" placeholder="Ваше сообщение" class="form-control"><?=$_SESSION['message']?></textarea><br>
+        <input type="text" name="username" placeholder="Введите имя" value="<?php if(!empty($_SESSION['username']))
+            echo $_SESSION['username'];?>" class="form-control"><br>
+        <div class="text-danger"><?php if(!empty($_SESSION["error_username"])) echo $_SESSION["error_username"];?></div><br>
+        
+        <input type="email" name="email" placeholder="Введите Email" value="<?php if(!empty($_SESSION['email']))
+            echo $_SESSION['email'];?>" class="form-control"><br>
+        <div class="text-danger"><?php if(!empty($_SESSION["error_email"])) echo $_SESSION["error_email"];?></div><br>
+        
+        <input type="text" name="subject" placeholder="Тема сообщения" value="<?php if(!empty($_SESSION['subject']))
+            echo $_SESSION['subject'];?>" class="form-control"><br>
+        <div class="text-danger"><?php if(!empty($_SESSION["error_subject"])) echo $_SESSION["error_subject"];?></div><br>
+        
+        <textarea name="message" placeholder="Ваше сообщение" class="form-control"><?php if(!empty($_SESSION['message']))
+                echo $_SESSION['message'];?></textarea><br>
+        <div class="text-danger"><?php if(!empty($_SESSION["error_message"])) echo $_SESSION["error_message"];?></div><br>
+        
         <button class="btn btn-success">Отправить</button>
     </form>
 <?php
--- mysite/controllers/check_contact.php
+++ mysite/controllers/check_contact.php
@@ -1,18 +1,50 @@
 <?php
 session_start();
 
-if ($_POST != ''){
+unset($_SESSION["username"]);
+unset($_SESSION['email']);
+unset($_SESSION['subject']);
+unset($_SESSION['message']);
+unset($_SESSION['error_username']);
+unset($_SESSION['error_message']);
+unset($_SESSION['error_email']);
+unset($_SESSION['error_subject']);
+function redirect()
+{
+    header("Location: ../contacts.php");
+    exit();
+}
 
+    if ($_POST != '') {
 $username = htmlspecialchars(trim($_POST['username']));
 $email = htmlspecialchars(trim($_POST['email']));
 $subject = htmlspecialchars(trim($_POST['subject']));
 $message = htmlspecialchars(trim($_POST['message']));
-
-//сохраняем в сесии
+    }
+//сохраняем в сессии
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
 $_SESSION['subject'] = $subject;
 $_SESSION['message'] = $message;
 
-header("Location: ../about.php");
+if (strlen($username) <= 1) {
+    $_SESSION["error_username"] = 'Введите корректное имя!';
+    redirect();
+} elseif (strlen($email) <= 3 || !(strpos($email, "@"))) {
+    $_SESSION["error_email"] = 'Вы ввели некорректный email';
+    redirect();
+} elseif (strlen($subject) < 3) {
+    $_SESSION["error_subject"] = 'Тема сообщения не может быть менее 3 символов';
+    redirect();
+} elseif (strlen($message) < 5) {
+    $_SESSION["error_message"] = 'Напишите подробнее';
+    redirect();
+} else {
+    $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+    $from = "example@mail.ru";
+    $headers = "From: $from\r\nReply-to: $from\r\nContent-type:text/plain; charset=utf-8\r\n";
+    mail("zhilzhil@mail.ru", $subject, $message, $headers);
+
+    $_SESSION['success_mail'] = "Вы успешно отправили письмо!";
+    redirect();
 }
